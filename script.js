e05a9e7a266f036faaf8502d965af46daf5c6b50
Miningflo <florian.debosscher@gmail.com>
2020-05-04 22:03:50 +0200

    Updated script

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ loadfile("./TestTLE/tle.txt").then(fulltle => {
         requestAnimationFrame(draw);
         drawnight();
         drawsats(tle.map(sat => {
+            // t is offset in minutes of current time
             let t = parseFloat(document.getElementById("slider").value);
             let date = new Date();
-            // t is offset in minutes of current time
             date = new Date(date.getTime() + t * 60000);
-            let loc = Constants.getLonLat(sat.sgp4(date).pos);
-            return new Sat(sat.satname, ol.proj.fromLonLat(loc), '#4362ff');
+            let position = sat.sgp4(date).pos;
+            let loc = Constants.getLonLat(position);
+            let colour = Constants.isLit(position, date) ? '#4362ff' : '#171b54';
+            return new Sat(sat.satname, ol.proj.fromLonLat(loc), colour);
         }));
         drawpaths(tle.map(sat => {
             let res = [];
             for (let i = 0; i < 360; i++) {
+                // t is offset in minutes of current time
                 let t = i / 360 * 1440 / sat.no;
                 t += parseFloat(document.getElementById("slider").value);
-                // t is offset in minutes of current time
                 let date = new Date();
                 date = new Date(date.getTime() + t * 60000);
                 res.push(Constants.getLonLat(sat.sgp4(date).pos));
